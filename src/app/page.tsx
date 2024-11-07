2c603509209ab198c39416b67b35fff16e361abe
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-11-07 09:52:12 +0530

    optimise lcp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,9 @@ export default function Home() {
   return (
     <div className=" min-h-[calc(100vh-4rem)] w-full justify-center">
       <div>
-        <section className="flex w-full items-center flex-col max-sm:items-start gap-4 p-10 ">
-          <h1 className="text-7xl font-semibold mt-10 tracking-normal text-wrap sm:text-[8rem] md:text-[9rem] lg:text-[12rem] xl:text-[14rem]">
-            Learn to <span className="text-emerald-500">Code.</span>
+        <section className="flex w-full flex-col max-sm:items-start gap-4 p-10 ">
+          <h1 className="text-7xl font-semibold mt-10 tracking-normal text-wrap sm:text-[8rem] md:text-[9rem] lg:text-[12rem] transition-all">
+            Learn to <br /><span className="text-emerald-500">Code.</span>
           </h1>
           <p className="font-semibold md:text-2xl">In the most concise way</p>
         </section>
