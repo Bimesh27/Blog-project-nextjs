aa707173772c405e465cfd72fad67e3ebbe1c8a3
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-11-08 14:14:28 +0530

    lcp optimise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import CourseWrapper from "@/components/CourseWrapper";
 
 export default function Home() {
-  
   return (
     <div className=" min-h-[calc(100vh-4rem)] w-full justify-center">
       <div>
         <section className="flex w-fit flex-col max-sm:items-start gap-4 p-10 ">
-          <h1 className="text-7xl font-semibold mt-10 tracking-normal text-wrap sm:text-[8rem] md:text-[9rem] lg:text-[12rem] transition-all">
+          <h1 className="text-7xl font-semibold mt-10 tracking-normal text-wrap sm:text-[8rem] md:text-[9rem] transition-all">
             Learn to <br />
             <span className="text-emerald-500">Code.</span>
           </h1>
